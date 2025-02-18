d7d7dda97216214290607ebe45d21bc291d8eec4
salocin-95 <renicolas7@gmail.com>
2025-02-18 14:16:03 +0000

    Modifiy files

--- tareas/clase-3/tarea-clase-3.js
+++ tareas/clase-3/tarea-clase-3.js
@@ -7,9 +7,29 @@
 // Imprimir "Hola " y el nombre, " te llamás igual que mi ..."
 // Si no, simplemente imprimir "Hola " + nombre!
 
+// const miNombre = "Rodrigo";
+
+// let nombreUsuario = prompt("Cuál es tu nombre?");
+
+// if (nombreUsuario === miNombre) {
+//     console.log("Hola, Tocayo! Yo tambien me llameo " + nombreUsuario);
+// }
+
 //Tarea 2:
 // Preguntar la edad del usuario
 // Hacerle saber si tiene más, menos ó la misma edad que nosotros.
+
+// const miEdad = "29";
+
+// let edadUsuario = prompt("Cual es tu edad?")
+
+// if (miEdad < edadUsuario) {
+//     console.log("Sos mayor que yo");
+// } else if (miEdad > edadUsuario) {
+//     console.log("Sos menor que yo");
+// } else {
+//     console.log("Tenemos la misma edad");
+// }
 
 //Tarea 3:
 // Preguntarle al usuario si tiene documento, y que conteste con "si" o "no".
@@ -19,3 +39,17 @@
 // Si no tiene documento, no dejarlo entrar al bar.
 // Si no entendemos la respuesta, le decimos que no entendimos la respuesta.
 // Punto bonus: SI, NO, Si, No, si, no.
+
+let documentoUsuario = prompt("Tenes documento?").toLowerCase();
+
+if (documentoUsuario === "no") {
+    console.log("No podes entrar porque no tenes documento")
+} else if (documentoUsuario === "si") {
+    let edadUsario = prompt("Cuantos años tenes?")
+    if (edadUsario > 18) {
+        console.log("Podes entrar al bar");
+    } else if (edadUsario < 18) {
+        console.log("No podes entrar al bar porque sos menor de")
+    }  
+} 
+
